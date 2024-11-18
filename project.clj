35df4cb3c21e1af4fa2b26fa93f2c91d5a7ba800
sxomile <milosprorocic1307@gmail.com>
2024-11-18 19:35:05 +0100

    midje added, but the test fails

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject wiring-material-calculator "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.12.0"]]
-  :main wiring-calculator.core
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [midje "1.10.9"]]
+  :plugins [[lein-midje "3.2.1"]]
+  :main ^:skip-aot wiring-calculator.core
 )
---
+++ test/wiring_calculator/core_test.clj
@@ -0,0 +1,16 @@
+(ns wiring-calculator.core-test
+  (:require [clojure.test :refer :all]
+            [midje.sweet :refer [facts => ]]
+            [wiring-calculator.core :refer :all]))
+
+;user wants to see available projects to check the layout of the construction site (apartment/warehouse etc.)
+
+(facts "load-project-names-test" load-existing-project-names => '("project1.txt" "project2.txt" "project3.txt"))
+
+
+
+
+
+
+
+
