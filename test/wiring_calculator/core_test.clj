705e2649a8f1796ae3260595b2f8dbcfabcc73b5
sxomile <milosprorocic1307@gmail.com>
2024-11-18 19:43:52 +0100

    test issue resolved

--- test/wiring_calculator/core_test.clj
+++ test/wiring_calculator/core_test.clj
@@ -5,7 +5,7 @@
 
 ;user wants to see available projects to check the layout of the construction site (apartment/warehouse etc.)
 
-(facts "load-project-names-test" load-existing-project-names => '("project1.txt" "project2.txt" "project3.txt"))
+(facts "load-project-names-test" (load-existing-project-names) => '("project1.txt" "project2.txt" "project3.txt"))
 
 
 
